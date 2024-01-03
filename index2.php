08535acae651421830496e007563c9d15bf1b95d
Amal-jawadi <waheb.jawadi037@gmail.com>
2024-01-03 21:23:21 +0100

    connect db

--- index2.php
+++ index2.php
@@ -6,7 +6,7 @@
     <title>Laws PractIice</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   </head>
- 
+ ghh
     </style>
   <body>
  <nav class="navbar navbar-expand-lg bg-transparent shadow p-3 mb-5 bg-body-light">  
