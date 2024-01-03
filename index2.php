6d16281c0bb5e6f0fdc61e94d69744178c405160
Amal-jawadi <waheb.jawadi037@gmail.com>
2024-01-03 21:01:30 +0100

    connect db

---
+++ index2.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  </head>
+  <style>
+    .a{
+  color: $yellow-300;
+  background-color: $indigo-900;
+}
+    </style>
+  <body>
+ <nav class="navbar navbar-expand-lg bg-transparent shadow p-3 mb-5 bg-body-light">  
+  <div class="container-fluid">
+  <img src="log2.png" alt="Logo" style="width:70px;">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#"></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"><p class="text-primary-emphasis">Home</p></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"><p class="text-primary-emphasis">lawyers</p></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"><p class="text-primary-emphasis">Specialties</p></a>
+</li>
+<li class="nav-item">
+          <a class="nav-link" href="#"><p class="text-primary-emphasis">Contacts</p></a>
+</li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-primary text-primary-emphasis" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  </body>
+</html>
