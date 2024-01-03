5b692293966720c7da0203848ac487058005056a
Amal-jawadi <waheb.jawadi037@gmail.com>
2024-01-03 21:04:12 +0100

    connect db

--- index2.php
+++ index2.php
@@ -3,14 +3,10 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Laws Practice</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   </head>
-  <style>
-    .a{
-  color: $yellow-300;
-  background-color: $indigo-900;
-}
+ 
     </style>
   <body>
  <nav class="navbar navbar-expand-lg bg-transparent shadow p-3 mb-5 bg-body-light">  
