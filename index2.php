c8a6ba171c97f6c7ab3ffc9a6ea0f9e3fa7ee992
Amal-jawadi <waheb.jawadi037@gmail.com>
2024-01-07 18:50:57 +0100

    connect db

--- index2.php
+++ index2.php
@@ -17,7 +17,7 @@ if (isset($_GET) && $_GET && $_GET['search']){
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Laws Practice</title>
+    <title>Lawyers of Now</title>
     <link rel="icon" href="logo.png" type="image/icon type">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
